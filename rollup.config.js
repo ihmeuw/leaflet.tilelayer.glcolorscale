ac1f424c8e3b1c6d24d2a7d8e8167a5f8f45ec3b
David Schneider <davschne@uw.edu>
2018-12-10 15:36:20 -0800

    Swap out Webpack in favor of Rollup.
    Rollup seems to be easier to configure, it does tree-shaking by default, and its generated code looks cleaner and more compact.

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import glslify from './rollup-plugin-glslify-cli';
+import postcss from 'rollup-plugin-postcss';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+
+import pkg from './package.json';
+
+const cwd = __dirname;
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    glslify(),
+    postcss({
+      plugins: [require('autoprefixer')],
+    }),
+    typescript({
+      clean: true,
+      typescript: require('typescript'),
+      useTsconfigDeclarationDir: true,
+    }),
+    sourcemaps(),
+  ],
+};
