f352319e1e41987a79b4f45e91204389e2631083
David Schneider <davschne@uw.edu>
2018-12-27 17:28:57 -0800

    Update Rollup config to add IIFE bundle output.

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,77 @@
-import glslify from './rollup-plugin-glslify-cli';
-import postcss from 'rollup-plugin-postcss';
-import typescript from 'rollup-plugin-typescript2';
+import autoprefixer from 'autoprefixer';
+import typescript from 'typescript';
+
+import minify from 'rollup-plugin-babel-minify';
+import commonjsPlugin from 'rollup-plugin-commonjs';
+import glslifyPlugin from './rollup-plugin-glslify-cli';
+import nodeResolvePlugin from 'rollup-plugin-node-resolve';
+import postcssPlugin from 'rollup-plugin-postcss';
+import typescriptPlugin from 'rollup-plugin-typescript2';
 
 import pkg from './package.json';
 
-const cwd = __dirname;
+const commonPlugins = [
+  glslifyPlugin(),
+  postcssPlugin({
+    plugins: [autoprefixer],
+  }),
+  typescriptPlugin({
+    clean: true,
+    typescript,
+  }),
+];
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      // CommonJS bundle
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      // ES Module bundle
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: commonPlugins,
+  },
+  // IIFE bundle, for use in a <script> tag. Dependencies other than Leaflet are bundled too.
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/bundle.min.js',
+      format: 'iife',
+      name: 'Leaflet.TileLayer',
+      globals: { leaflet: 'Leaflet' },
       sourcemap: true,
     },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [
-    glslify(),
-    postcss({
-      plugins: [require('autoprefixer')],
-    }),
-    typescript({
-      clean: true,
-      typescript: require('typescript'),
-    }),
-  ],
-};
+    external: [...Object.keys(pkg.peerDependencies || {})],
+    plugins: [
+      nodeResolvePlugin({
+        module: true,
+        jsnext: true,
+        main: false,
+        browser: true,
+        preferBuiltins: false,
+      }),
+      commonjsPlugin({
+        namedExports: {
+          'node_modules/upng-js/UPNG.js': ['decode'],
+        },
+      }),
+      ...commonPlugins,
+      minify({
+        comments: false,
+      }),
+    ],
+  },
+];
